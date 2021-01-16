b59cfc270a2eaa54e0e67c95ff8e7e77fb468cbc
claytonsilva <clayton@xdevel.com.br>
2021-01-16 15:27:17 -0300

    feat(iaac/localstack): added localstack endpoints in apigw

--- iaac/localstack/lambdas.tf
+++ iaac/localstack/lambdas.tf
@@ -1,9 +1,41 @@
-resource "aws_lambda_function" "todo" {
-  function_name = "todo_tf_handler"
-  handler       = "/var/task/dist/ports/aws-lambda/todo.handler"
+resource "aws_lambda_function" "talents_service" {
+  function_name = "talents_tf_handler"
+  handler       = "/var/task/dist/ports/aws-lambda/talents.handler"
   runtime       = "${var.runtime}"
   memory_size   = "128"
-  timeout       = 10                                             #for local environemnt this is larger than 10 seconds
+  timeout       = 15                                                #for local environemnt this is larger than 10 seconds
+
+  role      = "arn:aws:iam::123456:role/irrelevant"
+  s3_bucket = "__local__"
+  s3_key    = "${path.module}/../../"
+
+  environment = {
+    variables = "${local.environent}"
+  }
+}
+
+resource "aws_lambda_function" "openings_service" {
+  function_name = "openings_tf_handler"
+  handler       = "/var/task/dist/ports/aws-lambda/openings.handler"
+  runtime       = "${var.runtime}"
+  memory_size   = "128"
+  timeout       = 15                                                 #for local environemnt this is larger than 10 seconds
+
+  role      = "arn:aws:iam::123456:role/irrelevant"
+  s3_bucket = "__local__"
+  s3_key    = "${path.module}/../../"
+
+  environment = {
+    variables = "${local.environent}"
+  }
+}
+
+resource "aws_lambda_function" "stub" {
+  function_name = "stub_tf_handler"
+  handler       = "/var/task/dist/ports/aws-lambda/stub.handler"
+  runtime       = "${var.runtime}"
+  memory_size   = "128"
+  timeout       = 5                                              #for local environemnt this is larger than 10 seconds
 
   role      = "arn:aws:iam::123456:role/irrelevant"
   s3_bucket = "__local__"
--- iaac/localstack/provider.tf
+++ iaac/localstack/provider.tf
@@ -1,6 +1,11 @@
 provider "aws" {
   region = "${var.region}"
 
+  s3_force_path_style         = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
   endpoints {
     dynamodb   = "http://localhost:4566"
     s3         = "http://localhost:4566"
