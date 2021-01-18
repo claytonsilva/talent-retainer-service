6f180cfa4428943b67deb2a792474357e75f702f
claytonsilva <clayton@xdevel.com.br>
2021-01-18 03:00:10 -0300

    feat(business): create function to generate dynamo query

--- src/business/common.js
+++ src/business/common.js
@@ -1,7 +1,7 @@
 /**
  * common functions in business validation
  */
-import { not, is } from 'ramda'
+import { not, is, isEmpty, assoc } from 'ramda'
 
 /**
    * @description Validate if all objects in the array have same type of the argument
@@ -16,4 +16,19 @@ export const validateInnerArrayString = (data) => {
   }
 
   return not(data.some(tag => not(is(String, tag))))
+}
+
+/**
+   * @description Reduce array of tags in a query for usage with match method in opening/talent business
+   * @memberof business
+   * @function
+   * @param {Array} data array of the elements
+   * @returns {boolean}
+   */
+export const reduceContains = (fieldName) => (acc, tag, index) => {
+  // first or condition
+  return {
+    expression: acc.expression + `${isEmpty(acc.expression) ? '' : '\n        OR'} contains(${fieldName}, :${fieldName}${index})`,
+    value: assoc(`:${fieldName}${index}`, tag, acc.value)
+  }
 }
